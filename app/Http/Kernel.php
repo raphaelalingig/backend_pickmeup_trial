c661975f0319c4d1b0242cc0d039fdba9f0bfe54
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2025-02-09 19:45:28 +0800

    d

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
     ];
-
+ 
     /**
      * The application's route middleware groups.
      *
