3e4bd734773a7e371f7bb9b049946ca5b96aa029
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2025-02-09 19:15:57 +0800

    cors and sanc update

--- config/sanctum.php
+++ config/sanctum.php
@@ -19,14 +19,14 @@ return [
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
-        'pickmeup-admin.vercel.app',
+        'adminpickmeup.vercel.app',
     ))),
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
-        'pickmeup-admin.vercel.app',
+        'adminpickmeup.vercel.app',
     ))),
 
     /*
