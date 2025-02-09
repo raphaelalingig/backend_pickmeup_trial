4e311225b49e81338a4579da86760886aa84a3cb
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2025-02-09 19:40:57 +0800

    sanc

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000', 
